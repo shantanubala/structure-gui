06cd3297dfe1d0d3085f8a421c95620c73fefdf4
Shantanu Bala <shantanu@sbala.org>
2018-12-19 15:57:59 -0700

    emptied builds folder

--- Builds/release-x86_64/CMakeFiles/Samples.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Samples"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Samples.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Builds/release-x86_64/Samples/SimpleStreamer/CMakeFiles/copylib_StructureLinux_for_SimpleStreamer.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copylib_StructureLinux_for_SimpleStreamer"
-  "libStructureLinux.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copylib_StructureLinux_for_SimpleStreamer.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
